f1bfa705da15d84210b15f18af3eb03d9dfe3fc8
Swan Pham <accounting@nailsrus.ca>
2021-07-11 21:02:42 -0400

    create pages

--- src/component/Resume/index.js
+++ src/component/Resume/index.js
@@ -1,7 +1,47 @@
 function Resume() {
     return (
       <div>
-        <h2>Resume</h2>        
+        <nav>
+          
+            <ul>
+              <h2>
+              Swan Pham
+              </h2>              
+              
+              <li>Certified Full Stack Web Developer</li>
+
+              <li>
+                Phone: <a href="https://www.nailsrus.ca/tel:9056141008">905 614 1008</a>
+              </li>
+              <li>
+                Email: <a href="mailto:swan@nailsrus.ca">swan@nailsrus.ca</a>
+              </li>
+              <li>
+                Linkedin:<a href="https://www.linkedin.com/in/swan-pham-9428aa206/">linkedin</a>
+                                
+              </li>
+              <li>
+                Github:<a href="https://github.com/swanpham/portfolio">github</a>
+                                
+              </li>   
+                         
+            
+            </ul>
+            </nav>
+          <h2>Summary of Qualifications</h2>  
+          <h3>
+            Dedicated and efficient full stack developer experience in application layers, presentation layers, and databases. 
+          </h3>
+          <h2>
+          Work Experience
+          </h2>
+          <ul>
+            <li>
+            Full Stack Developer
+            </li>
+              <li>August 1 2021</li>
+            
+          </ul>
       </div>
     );
   }
