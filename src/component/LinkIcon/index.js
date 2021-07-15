5eb42c196a1e2bc6ba4d4681a02c261036c741be
Swan Pham <accounting@nailsrus.ca>
2021-07-14 22:42:03 -0400

    delete files

--- src/component/LinkIcon/index.js
+++
@@ -1,8 +0,0 @@
-const LinkIcon = ( { children, url } ) => {
-    return (
-        <a href={ url } target="_blank" className='link-icon' rel="noreferrer" >
-            { children }
-        </a>
-    )
-}
-export default LinkIcon
