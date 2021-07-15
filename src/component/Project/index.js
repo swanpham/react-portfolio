ce8c5e5698d70f9074cf1a0da53ea03714c660fb
Swan Pham <accounting@nailsrus.ca>
2021-07-14 22:21:38 -0400

    add styles

--- src/component/Project/index.js
+++ src/component/Project/index.js
@@ -1,12 +1,20 @@
+import { FaGithub } from 'react-icons/fa'
+import { BiLinkAlt } from 'react-icons/bi'
+import LinkIcon from '../LinkIcon'
 const Project = ( { title, description, url, image, github } ) => {
     return (
         <article className="work-container">
-            <a href={ url } className="work-image-link">
-                <span className="work-title">{ title }</span>
-                <img className="project-image" src={ require(`../../assets/cover/${ image }`).default } alt={`${title}`} />
-            </a>
-            <span className="work-desc">{ description }</span>
-            <a href={ github } className="work-github">GitHub Link</a>
+            <div className="project-content">
+            <LinkIcon url={ github }>
+                <FaGithub/>
+            </LinkIcon>
+            <LinkIcon url={ url }>
+                <BiLinkAlt/>
+            </LinkIcon>
+                <span className="work-desc">{ description }</span>
+            </div>
+            <span className="work-title">{ title }</span>
+            <img className="project-image" src={ require(`../../assets/cover/${ image }`).default } alt={`${title}`} />
         </article>
     )
 }
